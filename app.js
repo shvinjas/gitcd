8d544d69a23ead28f5a8f63b887b3ddf7973bf4b
Jo Sanchez <shvinjas@gmail.com>
2024-03-21 12:21:12 +1300

    second commit

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 //app.js
-const express = require("express")
+const express = require("express");
 const app = express();
 require("dotenv").config();
 
-app.get("/",(request,response)=>{
-    response.send("Enrollment  Accepted")
-})
+app.get("/", (request, response) => {
+  response.send("Enrollment  Accepted.Initiated");
+});
 
 var PORT = process.env.PORT || 8080;
 
-function selection(credentials,rating){
-    return credentials*rating
+function selection(credentials, rating) {
+  return credentials * rating;
 }
 
-
-app.listen(PORT, ()=>{
-    console.log(`server is listening in ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`server is listening in ${PORT}`);
+});
 
 module.exports = selection;
